0d485be291a18b9f854454bb5e99b891418dc817
Chuck Jungmann <chuck@cpjj.net>
2024-08-08 11:41:27 -0500

    Publicize ti_set_cursor_position, ti_hide_cursor, and ti_show_cursor for the convenience of programs using the library.

--- pager.h
+++ pager.h
@@ -127,4 +127,13 @@ ARV pager_scroll_home(DPARMS *parms);
  * @}
  */
 
+/**
+ * @defgroup termstuff utility functions
+ * @{
+ */
+void ti_set_cursor_position(int row, int col);
+void ti_hide_cursor(void);
+void ti_show_cursor(void);
+/** @} */
+
 #endif
